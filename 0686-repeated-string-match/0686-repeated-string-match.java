4f7762315cd64651ed01a746b16d07a399a5c73c
Shreyas Bagwe <126782359+Shreyasb1015@users.noreply.github.com>
2023-12-01 12:03:12 +0530

    Time: 7 ms (93.80%), Space: 41.2 MB (84.44%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,16 +1,45 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int index=b.length()/a.length();
-        StringBuilder str = new StringBuilder();
-        int count=0;
-        for(int i=0;i<=index+2;i++){
-            if(str.toString().contains(b))
-                return count;
-            else{
-                str.append(a);
-                count++;
+        int m = a.length();
+        int n = b.length();
+        int maxLen = m + n - 1;
+
+        int[] border = getBorder(b);
+        int i = 0, j = 0, t = 0;
+
+        while (i < maxLen && j < n) {
+            int index = i % m;
+            if (a.charAt(index) == b.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0) {
+                j = border[j];
+            } else {
+                i++;
             }
         }
-        return -1;
+        
+        int times = (int)(Math.ceil(i * 1.0 / m));
+        return j == n ? times : -1;
+    }
+
+    private int[] getBorder(String str) {
+        int n = str.length();
+        int[] border = new int[n + 1];
+
+        int left = 0, right = 1;
+        while (right < n) {
+            if (str.charAt(left) == str.charAt(right)) {
+                left++;
+                right++;
+                border[right] = left;
+            } else if (left > 0) {
+                left = border[left];
+            } else {
+                right++;
+            }
+        }
+
+        return border;
     }
 }
