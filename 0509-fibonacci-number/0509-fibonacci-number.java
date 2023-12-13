e35307966df56fa1b9cff828266e7b5f845eb01b
Shreyas Bagwe <126782359+Shreyasb1015@users.noreply.github.com>
2023-12-14 00:30:49 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (46.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,12 @@
 class Solution {
     public int fib(int n) {
-        
-        if(n==1) return 1;
-        if(n==0) return 0;
-        
-        return fib(n-1)+fib(n-2);
-        
+        if (n == 0) return 0;
+		int prev = 0,  next = 0, curr = 1;
+		for (int i = 1; i < n; i++) {
+			next = prev + curr;
+			prev = curr;
+			curr = next;
+		}
+		return curr;     
     }
 }
