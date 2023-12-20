d7d34f3cb8424db22443828833ad951c97fece2b
Shreyas Bagwe <126782359+Shreyasb1015@users.noreply.github.com>
2023-12-20 15:16:10 +0530

    Time: 5 ms (82.39%), Space: 51.9 MB (26.04%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,13 +1,9 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        List<Integer> ans = new ArrayList<>();
-        int num = 0;
-        for (int i = 0; i < (1 << n); i++) {
-            // Compute the XOR of the current index i with (i & -i) to generate the next gray code.
-            num ^= i & (-i);
-            // Add the generated gray code to the answer list.
-            ans.add(num);
+        List<Integer> l=new ArrayList<>();
+        for(int i=0;i<(1<<n);i++){
+            l.add(i^(i>>1));
         }
-        return ans;
+        return l;
     }
 }
