f12efcad12b8074f9782328c4cd1e7d743073213
Shreyas Bagwe <126782359+Shreyasb1015@users.noreply.github.com>
2024-01-01 10:22:20 +0530

    Time: 43 ms (41.47%), Space: 40.5 MB (27.14%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,28 +1,19 @@
-class Solution {
-    int dp[][],n;
-    public int countArrangement(int _n) {
-        n=_n;
-        if(n<=1) return n;
-        dp=new int[n+1][1<<(n+1)];
-        for(int i=0;i<=n;i++)
-            Arrays.fill(dp[i],-1);
-        return rec(1,0);
+ class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        boolean[] visited = new boolean[N + 1];
+        calculate(N, 1, visited);
+        return count;
     }
-    public int rec(int place,int state)
-    {
-        if(place==n+1)
-        {
-            return 1;
-        }
-        if(dp[place][state]!=-1) return dp[place][state];
-        int ans=0;
-        for(int i=1;i<=n;i++)
-        {
-            if((state & (1<<i))==0 && (i%place==0 || place%i==0))
-            {
-                ans+=rec(place+1,state|(1<<i));
+    public void calculate(int N, int pos, boolean[] visited) {
+        if (pos > N)
+            count++;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+                visited[i] = true;
+                calculate(N, pos + 1, visited);
+                visited[i] = false;
             }
         }
-        return dp[place][state]=ans;
     }
 }
